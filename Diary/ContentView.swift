7af0c59263a2161c15aa6dc4973afed74063e271
UTakuto <taku.0307.ue@icloud.com>
2024-08-28 15:22:01 +0900

    Initial Commit

---
+++ Diary/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Diary
+//
+//  Created by 上森拓翔 on 2024/08/28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Diary/DiaryApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiaryApp.swift
+//  Diary
+//
+//  Created by 上森拓翔 on 2024/08/28.
+//
+
+import SwiftUI
+
+@main
+struct DiaryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
