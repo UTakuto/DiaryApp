fe750894d7bdf97137bea6d13cc9ab5c56b2fd60
HondaYt <honda.yuto.04@gmail.com>
2024-08-31 06:22:39 +0900

    キャラクターをコンポーネント化

---
+++ Diary/CharacterView.swift
@@ -0,0 +1,18 @@
+//
+//  CharacterView.swift
+//  Diary
+//
+//  Created by 本田有人 on 2024/08/31.
+//
+
+import SwiftUI
+
+struct CharacterView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CharacterView()
+}
