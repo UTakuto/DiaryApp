2b8589ca07624d34ba5d7b7b37c6fc03dd4e8091
UTakuto <taku.0307.ue@icloud.com>
2024-08-30 17:13:44 +0900

    first commit

---
+++ Diary/Input.swift
@@ -0,0 +1,18 @@
+//
+//  Input.swift
+//  Diary
+//
+//  Created by 上森拓翔 on 2024/08/30.
+//
+
+import SwiftUI
+
+struct Input: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+#Preview {
+    Input()
+}
