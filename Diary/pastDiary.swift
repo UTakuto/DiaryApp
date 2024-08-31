4c32a36d4d3c06150d0d573975e1ba5556adb658
HondaYt <honda.yuto.04@gmail.com>
2024-08-31 11:18:56 +0900

    エラーの修正

--- Diary/pastDiary.swift
+++ Diary/pastDiary.swift
@@ -7,33 +7,6 @@
 
 import SwiftUI
 
-struct HexagonShape: Shape {
-    func path(in rect: CGRect) -> Path {
-        let width = rect.size.width
-        let height = rect.size.height
-        let centerX = rect.midX
-        let centerY = rect.midY
-        
-        let radius = min(width, height) / 2
-        let angle = 60.0 * (Double.pi / 180.0)
-        
-        var path = Path()
-        
-        for i in 0..<6 {
-            let x = centerX + radius * CGFloat(cos(angle * Double(i)))
-            let y = centerY + radius * CGFloat(sin(angle * Double(i)))
-            if i == 0 {
-                path.move(to: CGPoint(x: x, y: y))
-            } else {
-                path.addLine(to: CGPoint(x: x, y: y))
-            }
-        }
-        
-        path.closeSubpath()
-        
-        return path
-    }
-}
 
 struct TopRoundedCorners: Shape {
     var radius: CGFloat = 8.0
