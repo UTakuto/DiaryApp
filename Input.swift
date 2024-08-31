6451ea6ea6c22fd9f058fb035e9dd19941949354
HondaYt <honda.yuto.04@gmail.com>
2024-08-31 12:39:28 +0900

    ページ遷移を改善

--- Input.swift
+++ Input.swift
@@ -1,22 +1,22 @@
 import SwiftUI
 
 struct Input: View {
-    @State private var accentColor = Color(red: 0/255, green: 130/255, blue: 153/255)
+    @State private var accentColor = Color(red: 0 / 255, green: 130 / 255, blue: 153 / 255)
     @State private var offsetY: CGFloat = 0
     @State private var movingDown = true
     let timer = Timer.publish(every: 2, on: .main, in: .common).autoconnect()
     
     @State private var userInput: String = ""
-    @State private var currentMessage: String? = "今日はどんな1日だった？" // 初期の質問
-    @State private var showNextScreen = false // 次の画面に遷移するためのフラグ
-    @State private var showWhiteScreen = false // 白い画面を表示するためのフラグ
-
+    @State private var currentMessage: String? = "今日はどんな1日だった？"  // 初期の質問
+    @State private var showNextScreen = false  // 次の画面に遷移するためのフラグ
+    @State private var showWhiteScreen = false  // 白い画面を表示するためのフラグ
+    
     struct AnimationValues {
         var scale: CGFloat
     }
     
     var body: some View {
-        NavigationView { // NavigationViewを追加
+        NavigationStack {  // NavigationViewをNavigationStackに変更
             ZStack {
                 Image("background")
                     .resizable()
@@ -58,21 +58,21 @@ struct Input: View {
                             }
                             .padding()
                         }
-
+                        
                         // TextInputViewをここで使用
                         TextInputView(
                             userInput: $userInput,
                             onSend: {
                                 // ボタンが押されたときのアクション
                                 if currentMessage == "今日はどんな1日だった？" {
-                                    currentMessage = "それは楽しかったね。詳しく教えて。" // 次の質問に更新
-                                    userInput = "" // 入力フィールドをクリア
+                                    currentMessage = "それは楽しかったね。詳しく教えて。"  // 次の質問に更新
+                                    userInput = ""  // 入力フィールドをクリア
                                 } else if currentMessage == "それは楽しかったね。詳しく教えて。" {
-                                    withAnimation(.easeInOut(duration: 1)) { // 1秒かけて白い画面にフェードイン
+                                    withAnimation(.easeInOut(duration: 1)) {  // 1秒かけて白い画面にフェードイン
                                         showWhiteScreen = true
                                     }
                                     DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
-                                        showNextScreen = true // 1秒後に次の画面に遷移
+                                        showNextScreen = true  // 1秒後に次の画面に遷移
                                     }
                                 }
                             },
@@ -89,20 +89,12 @@ struct Input: View {
                     Color.white
                         .ignoresSafeArea()
                         .transition(.opacity)
-                        .animation(.easeInOut(duration: 1), value: showWhiteScreen) // 1秒かけてフェードイン
                 }
-
-                // フェードで次の画面に遷移
-                if showNextScreen {
-                    NavigationLink(
-                        destination:CompleteView(), // 新しいファイルに定義された画面に遷移
-                        isActive: $showNextScreen,
-                        label: {
-                            EmptyView() // ここでは空のビューを使用
-                        }
-                    )
-                    .transition(.opacity) // フェードアニメーションを設定
-                }
+            }
+            .navigationBarBackButtonHidden(true)
+            .animation(.easeInOut(duration: 1), value: showWhiteScreen)
+            .navigationDestination(isPresented: $showNextScreen) {
+                CompleteView()
             }
         }
     }
